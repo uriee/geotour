927b08afdf70fee5edfd9fb1764425801ef9a011
uri even ezra <uevenezra@gmail.com>
2014-09-21 15:11:18 +0000

    GOT ORGENIZED

--- index.js
+++ index.js
@@ -8,11 +8,38 @@ var Q = require("q");
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-//app.use(express.static(__dirname + '/scripts'));
 var io = require('socket.io')(http);
 
+app.use(express.static(__dirname + '/scripts'));
+app.engine('.html', require('ejs').__express);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+
+app.get('/init', function(req, res){
+  res.render('init', {
+    users: 4,
+    title: "EJS example",
+    header: "Some users"
+  });
+});
+
 app.get('/', function(req, res){
-  res.sendfile('demo2.html');
+  res.render('welcome');
+});
+
+app.get('/tour/:tourname/:username', function(req, res){
+  data = {}
+  Get('tour:'+req.params.tourname).then(function(d){
+    res.render('tour', {
+      tourname :  req.params.tourname,
+      username :  req.params.username,
+      H : d.H,
+      M : d.M,
+      lat : d.lat,
+      lng : d.lng
+    });    
+  });
+
 });
 
 var Smembers = function(key){
@@ -36,15 +63,6 @@ var Sadd = function(key,val){
 var Set = function(key,val){
   var d = Q.defer();
   client.set(key,val,function(err,ret){
-    if(err) {console.log("Err set",key,val,err); d.reject(err);}
-    else d.resolve(ret);
-  });
-  return d.promise;
-};
-
-var Hmset = function(key,val){
-  var d = Q.defer();
-  client.hmset(key,val,function(err,ret){
     if(err) {console.log("Err hmset",key,val,err); d.reject(err);}
     else d.resolve(ret);
   });
@@ -75,15 +93,16 @@ io.on('connection', function(socket){
     console.log('user disconnected');
   });  
   socket.on('addTour', function (data) {
-    console.log(data,JSON.parse(data));
+    console.log("kkk:",data,JSON.parse(data));
     var inp = JSON.parse(data);
-    Hmset('tour:'+inp.name,inp.args).then(/*redirection*/);
+    console.log('Hmset(tour:','tour:'+inp.name,inp.args);
+    Set('tour:'+inp.name,JSON.stringify(inp.args)).then(run(inp.name));
   });
   socket.on('addUser', function (data) {
     console.log(data,JSON.parse(data));
     var inp = JSON.parse(data);
     Sadd('tourUsers:'+inp.tour,inp.user);
-    Hmset('user:'+inp.user,inp.args);
+    Set('user:'+inp.user,inp.tour);
   });
   socket.on('updateLocation', function (data) {
     console.log(data,JSON.parse(data));
@@ -120,64 +139,28 @@ http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
-addTerms = function(user,term,cb) {
-  console.log("in incT :",user,term);
-  client.smembers('userTerms:'+user,function(err,ret){
-    if(err) cb(1,0);
-    console.log("in incT smembers:",ret);  
-    ret.forEach(function(T){
-      if(T != term) {
-         client.sadd('termR:'+term, T,function(err,ret){
-            if(err) cb(1,0);
-            client.sadd('termR:'+T,term,function(err,ret){
-              if(err) cb(1,0);            
-            }); 
-         });
-      }
-    })
-    cb(0,1);
-  });
-}; 
 
-addUsers = function(user,term,cb) {
-  console.log("in incU :",user,term);
-  client.smembers('userTerms:'+user, function(err,ret){
-    if (err) cb(1,0);  
-    console.log("in incU smembers:",err,ret);
-    ret.forEach(function(U) {
-      if(U != user) {
-        console.log("in incU : fi",U);
-        client.sadd('userR:'+user, U,function(err,ret){
-          if(err) cb(1,0);
-          client.sadd('userR:'+U, user,function(err,ret){
-            if(err) cb(1,0);
-          });
-        });
+var run  = function(tour){
+  var tourInterval = setInterval(function(){
+    var h = -2,
+        m = 0;
+    Get('tour:'+tour).then(function(data){
+      data = JSON.parse(data);
+      var minuts = data.M,
+          houres = data.H;
+      minuts--;
+      if(minuts == -120) clearInterval(tourInterval);
+      if(minuts === 0 & houres === 0) {
+        /* time ended */
       }
-    });
-  });
-}
-         
- 
-
-insertTerm = function(user,term,cb){
-  console.log("in IT",user,term);
-  client.sadd("termUsers:"+term, user,function(err,ret){
-    if(err) cb(1,0);
-    client.sadd("userTerms:"+user, term,function(err,ret){
-      if(err) cb(1,0);
-      client.incr('term:'+term,function(err,ret){
-        console.log(err,ret);
-        if(err) cb(1,0);
-        addTerms(user,term,function(err,ret){
-          if(err) cb(1,0);
-          addUsers(user,term,function(err,ret){
-            console.log("incUsers called back");
-            if(err) cb(1,0);
-            cb(0,1)
-          });
-        });
-      });  
+      if(minuts == -1 & houres > 0){
+        minuts=59;
+        houres--;
+      }
+      data.M = minuts;
+      data.H = houres;
+      Set('tour:'+tour,JSON.stringify(data)).then();
     });  
-  });
+  }, 1000);
 }
+
